347a53e9daf6d7c3f7d807f65f036ac6e18930d2
Philipp Page <github@philipp-page.de>
2021-05-16 14:27:14 +0200

    Rewrite constructor to function constructor in src/models/utils.ts

--- src/models/utils.ts
+++ src/models/utils.ts
@@ -1,7 +1,7 @@
 import produce, { Draft } from "immer";
 
 export function createModelFromPlain<Type extends { id: string }>(
-  model: { new (plain: Type): Type },
+  model: new (plain: Type) => Type,
   plain: Type
 ): Type {
   return produce(new model(plain), (model: Draft<Type>) => {
