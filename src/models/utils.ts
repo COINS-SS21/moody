0fbda1533693b8ea41bd0051cea390d2b9d98d41
Philipp Page <github@philipp-page.de>
2021-05-20 20:03:37 +0200

    Bugfix: Immer.js ignores _ fields if they are not explicitely set.

--- src/models/utils.ts
+++ src/models/utils.ts
@@ -1,10 +1,17 @@
 import produce, { Draft } from "immer";
 
-export function createModelFromPlain<Type extends { id: string }>(
+export function createModelFromPlain<Type>(
   model: new (plain: Type) => Type,
   plain: Type
 ): Type {
   return produce(new model(plain), (model: Draft<Type>) => {
+    // @ts-ignore
     model.id = plain.id;
+    // @ts-ignore
+    model._deleted = plain._deleted;
+    // @ts-ignore
+    model._lastChangedAt = plain._lastChangedAt;
+    // @ts-ignore
+    model._version = plain._version;
   });
 }
